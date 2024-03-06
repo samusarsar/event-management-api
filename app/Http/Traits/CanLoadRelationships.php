73bf2b0a80984e48f1b18c581183c49215c248f9
samu.sar <samuil.sarandev@gmail.com>
2024-03-06 16:05:45 +0200

    Add: implemented CanLoadRelationships trait in AttendeeController

--- app/Http/Traits/CanLoadRelationships.php
+++ app/Http/Traits/CanLoadRelationships.php
@@ -5,10 +5,11 @@ namespace App\Http\Traits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait CanLoadRelationships
 {
-    public function loadRelationships(Model | QueryBuilder | EloquentBuilder $for, ?array $relations = null): Model | QueryBuilder | EloquentBuilder
+    public function loadRelationships(Model | QueryBuilder | EloquentBuilder | HasMany $for, ?array $relations = null): Model | QueryBuilder | EloquentBuilder | HasMany
     {
         $relations = $relations ?? $this->relations ?? [];
 
